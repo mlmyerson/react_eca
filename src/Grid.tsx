05c99100dad5b84b5dea9c0ee43e2f03f966edf2
mmyerson <mlmyerson@gmail.com>
2025-07-12 11:42:40 -0400

    2d array of cells

--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,39 +1,32 @@
-import { type JSX, useState, useEffect} from 'react'
+import { useState, useEffect } from 'react';
 import styles from './Grid.module.css';
-import * as React from "react";
 
 interface GridProps {
   grid_width: number;
   grid_height: number;
 }
 
-export default function Grid({grid_width, grid_height}: GridProps):JSX.Element {
+export default function Grid({ grid_width, grid_height }: GridProps) {
+  const [width, setWidth] = useState(grid_width);
+  const [height, setHeight] = useState(grid_height);
+  const cells = Array.from({ length: width * height });
 
+  useEffect(() => {
+    setWidth(grid_width);
+    setHeight(grid_height);
+  }, [grid_width, grid_height]);
 
-    const [width, setWidth] = useState(grid_width)
-    const [height, setHeight] = useState(grid_height)
-    const cells = Array.from({length: grid_width * grid_height})
-
-
-    useEffect((): void => {
-        setWidth(grid_width)
-        setHeight(grid_height)
-    }, [grid_width, grid_height])
-
-
-    return (
-        <div
-            className="grid-container"
-            style={
-                {
-                  '--columns': width,
-                  '--rows': height,
-                } as React.CSSProperties
-            }
-        >
-            {cells.map((_, idx) => (
-                <div className={styles.cell} key={idx} />
-                ))}
-        </div>
-    )
+  return (
+    <div
+      className={styles.container}
+      style={{
+        '--columns': width,
+        '--rows': height,
+      } as React.CSSProperties}
+    >
+      {cells.map((_, idx) => (
+        <div className={styles.cell} key={idx} />
+      ))}
+    </div>
+  );
 }
